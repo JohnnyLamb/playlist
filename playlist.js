841a2c0d72923f0e5f127d689e529b82e818ae71
johnny lamb <agnusjack@gmail.com>
2015-07-24 11:39:03 -0600

    first commit

--- playlist.js
+++ playlist.js
@@ -1,14 +1,49 @@
-var Song = require('./song');
+// var Song = require('./song');
 
-var Playlist = function(){
+// var Playlist = function(){
+// };
+
+// Playlist.prototype.isEmpty = function () {
+// };
+
+
+// Playlist.prototype.forNumber = function () {
+
+// };
+
+// module.exports = Playlist;
+
+// make songs for playlist
+var Song = function(title,artist,length){
+  this.title = title;
+  this.artist = artist;
+  this.length = lenght;
+};
+// songs have titles artist and length
+
+// playlist needs a name
+var Playlist = function(name){
+  this.name = name;
+  this.songs = [];
+  this.current.song = null;
 };
 
-Playlist.prototype.isEmpty = function () {
-};
-
-
-Playlist.prototype.forNumber = function () {
+Playlist.prototype.addSong = function(song){
+  this.songs.push(song);
 
 };
 
-module.exports = Playlist;
+Playlist.prototype.getArtists = function(){
+  var artists = [];
+  for(i = 0; i < this.songs.length; i++){
+    artists.push((this.song[i]).artist);
+  }
+
+};
+
+
+// remove songs
+// play/next/stop
+// shuffle songs
+
+
